a75d1e5eef9a2656dfc3895abc7aec92dff4b3cd
2021110806 <2021110806@dgu.ac.kr>
2024-09-06 19:15:32 +0900

    2

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -5,14 +5,20 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
 class BitcoinExchange
 {
 	private:
 		std::map<std::string, float> info;
 		void	readUserInput();
-		bool	ValidateInputTxtFile();
+		bool	ValidateInputTxtFile(std::string &fileName);
 		bool	ValidateCsvFile();
+		bool 	ValidateDate(int year, int month, int day);
 		void	parse();
+		void 	csvToMap(std::stringstream &line);
 		void	calcaculate();
 		void	printResult();
 
